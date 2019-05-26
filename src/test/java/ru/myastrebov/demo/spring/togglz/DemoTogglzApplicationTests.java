866333a21636854c8611f9f62ce5896a7dabc4ad
Yastrebov Maxim <yamax343@gmail.com>
2019-05-26 17:19:00 +0700

    first commit

---
+++ src/test/java/ru/myastrebov/demo/spring/togglz/DemoTogglzApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.myastrebov.demo.spring.togglz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTogglzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
